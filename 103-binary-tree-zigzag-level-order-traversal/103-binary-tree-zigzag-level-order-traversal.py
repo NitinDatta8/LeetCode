4548065384373cc76308ea77df5e54ce83722549
Nitin Datta <movvand@gmail.com>
2022-01-15 16:30:18 +0530

    Time: 32 ms (77.79%), Space: 14.4 MB (71.54%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -1,32 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        def bfs(root):
-            if root is None: return []
-            if root.val is None: return []
-            queue = [root]
-            final_result = []
-            count = 0 
-            while len(queue)>0:
-                count += 1
-                result = []
-                for i in range(len(queue)):
-                    current = queue.pop(0)
-                    result.append(current.val)
-                    if current.left is not None: queue.append(current.left)
-                    if current.right is not None: queue.append(current.right)
-                    
-                    
-                if count%2!=0:     
-                    final_result.append(result)  
-                else:
-                    final_result.append(result[::-1])
-            return final_result
+        if not root: return []
+        q = deque([root])
+        result = []
+        level = 0
         
-        out = bfs(root)
-        return out
+        while q:
+            currentnode = deque()
+            levelsize = len(q)
+            for _ in range(levelsize):
+                node = q.popleft()
+                if level % 2 == 0:
+                    currentnode.append(node.val)
+                else:
+                    currentnode.appendleft(node.val)
+                    
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            result.append(currentnode)
+            level += 1
+        return result
