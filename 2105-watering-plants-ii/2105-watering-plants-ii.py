4b39201d10b56bfdb20da77df696cc27054edac4
Nitin Datta <movvand@gmail.com>
2022-01-15 09:15:18 +0530

    Time: 894 ms (50.23%), Space: 29.2 MB (93.11%) - LeetHub

---
+++ 2105-watering-plants-ii/2105-watering-plants-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumRefill(self, plants: List[int], capacityA: int, capacityB: int) -> int:
+        ans = 0 
+        lo, hi = 0, len(plants)-1
+        canA, canB = capacityA, capacityB
+        while lo < hi: 
+            if canA < plants[lo]: ans += 1; canA = capacityA
+            canA -= plants[lo]
+            if canB < plants[hi]: ans += 1; canB = capacityB
+            canB -= plants[hi]
+            lo, hi = lo+1, hi-1
+        if lo == hi and max(canA, canB) < plants[lo]: ans += 1
+        return ans 
