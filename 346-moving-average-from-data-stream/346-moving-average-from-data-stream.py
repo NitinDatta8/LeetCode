9a8f6a7ed2b472db5daf4f4cc01ceb47143e28c4
Nitin Datta <movvand@gmail.com>
2022-01-17 17:36:43 +0530

    Time: 84 ms (57.86%), Space: 17.5 MB (14.58%) - LeetHub

--- 346-moving-average-from-data-stream/346-moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.py
@@ -1,19 +1,12 @@
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.window = size 
-        self.count = 0 
-        self.list = []
+        self.queue = collections.deque(maxlen=size)
         
     def next(self, val: int) -> float:
-        average = 0
-        self.list.append(val)
-        self.count+= 1
-        if self.count <= self.window:  
-            average = sum(self.list[-self.window:])/self.count
-        else:
-            average = sum(self.list[-self.window:])/self.window
-        return average
+        q = self.queue
+        q.append(val)
+        return float(sum(q)/len(q))
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)
