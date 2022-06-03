2368643b6c74531ceb96ca20443f07040b48756e
Nitin Datta <movvand@gmail.com>
2022-06-03 00:08:42 -0700

    Time: 905 ms (71.79%), Space: 18.2 MB (41.24%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,30 +1,27 @@
+# Optimized solution
+# 1. sort the list in ascending order 
+# 2. use for loop to iterate over all elements - if ith and i-1th element is same then continue 
+# 3. then use 2 pointer L and R to find triples that add to 0 --> add them to the result list 
+# 4. Update the pointers carefully 
 
 class Solution:
-    from itertools import combinations
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: List[int]) -> List[List[int]]: 
         res = []
-        val = 0 
         nums.sort()
-        for i in range(len(nums)-2):
-            
-            if i>0 and nums[i] == nums[i-1]:
+        for i, val in enumerate(nums):
+            if i > 0 and val == nums[i-1]:
                 continue
-            
-            l = i+1
-            r = len(nums)-1
-            while l<r: 
-                check_zero = nums[i] + nums[l] + nums[r]
-                if check_zero == 0: 
-                    res.append([nums[i],nums[l],nums[r]])
-                    while l<r and nums[l] == nums[l+1]:
-                        l += 1
-                    while l<r and nums[r] == nums[r-1]:
-                        r -= 1
+                
+            l, r = i+1, len(nums)-1
+            while l < r: 
+                three_sum = val + nums[l] + nums[r]
+                if three_sum > 0: 
+                    r -= 1
+                elif three_sum < 0: 
                     l += 1
-                    r -= 1
-                elif check_zero > 0: 
-                    r -= 1
                 else: 
+                    res.append([val, nums[l], nums[r]])
                     l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
         return res
-                    
