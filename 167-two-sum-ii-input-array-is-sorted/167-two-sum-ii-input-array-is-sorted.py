482e92dd90366c641d47b08977777e48d54a26de
Nitin Datta <movvand@gmail.com>
2022-06-02 21:33:45 -0700

    Time: 213 ms (29.88%), Space: 15.3 MB (6.94%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,17 +1,13 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        l,r = 0, len(nums)-1
-        while l<r: 
-            if nums[l] + nums[r] == target: 
-                return [l+1,r+1]
-            elif nums[l] + nums[r] > target: 
-                r -= 1
+# Better solution 
+# Hashmap - store index as value and element as the key 
+# check if target - element is present already as a key : YES then return current index+1 and value(index) of target-element  
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        freq = {}
+        for i in range(len(numbers)): 
+            if target - numbers[i] in freq: 
+                return [freq[target-numbers[i]]+1,i+1] 
             else: 
-                l += 1
-        
-            
+                freq[numbers[i]] = i
