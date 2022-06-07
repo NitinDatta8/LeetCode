be4887c2b60df43ed1f4ec54b879238183da80e4
Nitin Datta <movvand@gmail.com>
2022-06-07 12:09:33 -0700

    Time: 895 ms (72.67%), Space: 18.1 MB (71.67%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,19 +1,17 @@
-# Optimized solution
-# 1. sort the list in ascending order 
-# 2. use for loop to iterate over all elements - if ith and i-1th element is same then continue 
-# 3. then use 2 pointer L and R to find triples that add to 0 --> add them to the result list 
-# 4. Update the pointers carefully 
-
+# sort the nums 
+# iterate over nums and inside use while loop with l and r pointers to get the rest 2 numbers that add to 0 
+# check for duplicates and remove them 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]: 
         res = []
         nums.sort()
+        
         for i, val in enumerate(nums):
             if i > 0 and val == nums[i-1]:
                 continue
                 
             l, r = i+1, len(nums)-1
-            while l < r: 
+            while l < r:
                 three_sum = val + nums[l] + nums[r]
                 if three_sum > 0: 
                     r -= 1
@@ -21,7 +19,7 @@ class Solution:
                     l += 1
                 else: 
                     res.append([val, nums[l], nums[r]])
-                    l += 1
+                    l += 1 
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
         return res
