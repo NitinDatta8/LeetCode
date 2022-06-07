832b1fa069e0553174caffb8984e9b2476e03067
Nitin Datta <movvand@gmail.com>
2022-06-07 12:26:06 -0700

    Time: 1020 ms (41.80%), Space: 27.5 MB (15.03%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,31 +1,17 @@
-# Brute force 
-# Use 2 for loops to find all possible pairs 
-# for every pair find the minimum and multiply with j - i (x axis length)
-# Maintain a max_water variable that keeps updating if it finds a new maximum 
-
-# class Solution:
-#     def maxArea(self, height: List[int]) -> int:
-#         max_water = -1 
-#         for i in range(len(height)):
-#             for j in range(i+1, len(height)):
-#                 min_height = min(height[i], height[j])
-#                 water = min_height * (j - i)
-#                 max_water = max(max_water, water)
-#         return max_water
-    
-# Optimal solution 
-# use 2 pointers - 
-# 1. go from left to right and find minimum(l,r) and reduce from right 
+# use 2 pointers l and r 
+# find min of the 2 heights -> multiply with distance on x axis (r - l + 1)
+# if l < r: l ++ elif l > r: r-- 
+# maintain maxwater
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height)-1
-        max_water = -1 
+        maxwater = -1 * math.inf
         while l < r: 
-            min_height = min(height[l], height[r])
-            water = min_height * (r - l)
-            max_water = max(max_water, water)
-            if height[l] > height[r]:
-                r -= 1
-            else: 
+            water = min(height[l], height[r]) * (r - l)
+            maxwater = max(maxwater, water)
+            
+            if height[l] < height[r]:
                 l += 1
-        return max_water
+            else: 
+                r -= 1
+        return maxwater
