009869951b5d84c6972774f01ad5eade502a8d38
Nitin Datta <movvand@gmail.com>
2022-06-17 16:51:40 -0700

    Time: 56 ms (60.93%), Space: 13.9 MB (24.10%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,34 +1,29 @@
-# 1 3 5 4 3 2 1 
-# we know index 1 needs to be swapped because right side of it everything is sorted in decreasing order 
-# so index 1 or 3 is the pivot point 
-# now start from right and find a point such that its greater than the pivot and swap with it 
-# 1 4 5 3 3 2 1 
-# now sort everything in ascending order after the pivot which should be next biggest number
+# 1 3 5 4 3 2 1
+# 1 4 5 3 3 2 1
+# iterate backwards and check where the number is lesser 
+# that is pivot if pivot is 0 nums.sort() and return 
+# then again start from end and check where value > pivot 
+# swap pivot with that value
+# sort the numbers after pivot
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        pivot = 0
-        # find the pivot 
-        for i in range(n-1,0,-1):
-            if nums[i-1] < nums[i]:
-                pivot = i 
-                break
-        
+        pivot = 0 
+        for i in range(len(nums)-1, 0, -1): 
+            if nums[i-1] < nums[i]: 
+                pivot = i
+                break 
+                
         if pivot == 0: 
             nums.sort()
-            return
+            return 
         
+        r = len(nums)-1
+        while nums[pivot-1] >= nums[r]:
+            r -= 1
         
-        # find the swap which is first number greater than pivot 
-        swap = n-1
-        while nums[pivot-1] >= nums[swap]: 
-            swap -= 1
-        
-        
-        # swap 
-        nums[swap], nums[pivot-1] = nums[pivot-1], nums[swap]
-        # sort from pivot
+        nums[pivot-1], nums[r] = nums[r], nums[pivot-1]
         nums[pivot:] = sorted(nums[pivot:])
+                
