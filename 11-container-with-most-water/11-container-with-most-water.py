6a714420813850c3f798be8f3ac2bb13bf9819c1
Nitin Datta <movvand@gmail.com>
2022-06-17 17:41:22 -0700

    Time: 1049 ms (44.63%), Space: 27.5 MB (15.00%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,17 +1,20 @@
-# use 2 pointers l and r 
-# find min of the 2 heights -> multiply with distance on x axis (r - l + 1)
-# if l < r: l ++ elif l > r: r-- 
-# maintain maxwater
+# l r 
+# 1,8,6,2,5,4,8,3,7 
+# l               r
+# min(l,r) * (r-l)
+# if l < r: l ++ else r -- 
+# max_area = max(max_area, area)
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        area = 0 
+        max_area = -1
         l, r = 0, len(height)-1
-        maxwater = -1 * math.inf
         while l < r: 
-            water = min(height[l], height[r]) * (r - l)
-            maxwater = max(maxwater, water)
-            
-            if height[l] < height[r]:
+            min_height = min(height[l],height[r])
+            area = min_height * (r - l) 
+            max_area = max(max_area, area)
+            if height[l] < height[r]: 
                 l += 1
             else: 
                 r -= 1
-        return maxwater
+        return max_area
