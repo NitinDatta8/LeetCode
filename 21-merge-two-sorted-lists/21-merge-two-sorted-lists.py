69223a6bc8482bae274bf7360f13c1ad4e57b97e
Nitin Datta <movvand@gmail.com>
2022-06-23 10:47:04 -0700

    Time: 88 ms (5.08%), Space: 13.9 MB (31.77%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -9,13 +9,12 @@ class Solution:
         while list1 and list2: 
             if list1.val < list2.val: 
                 cur.next = list1
-                list1 = list1.next 
+                list1 = list1.next
             else: 
-                cur.next = list2 
-                list2 = list2.next 
-            cur = cur.next 
-            
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
         cur.next = list1 or list2
-        return dummy.next
             
+        return dummy.next 
             
