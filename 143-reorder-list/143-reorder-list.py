615c34a1471e43dda9db6251946a8e0e79a41b15
Nitin Datta <movvand@gmail.com>
2022-06-23 11:15:19 -0700

    Time: 199 ms (5.51%), Space: 24.3 MB (14.22%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -3,30 +3,43 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+1 -> 2 <- 3 <- 4
+          SE    
+1 -> 4 -> 2 -> 3
+
+1. use fast slow pointers to find the middle of list 
+2. as fast pointer will be on end of list use it and move reverse till slow pointer to reverse the list 
+3. start from start and end 
+'''
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow = head 
-        fast = head.next
-        while fast and fast.next: 
-            fast = fast.next.next 
-            slow = slow.next
-                
-        second = slow.next
-        prev = slow.next = None
-        while second: 
-            tmp = second.next 
-            second.next = prev
-            prev = second 
-            second = tmp
+        nums = []
+        cur = head 
+        while cur: 
+            nums.append(cur.val)
+            cur = cur.next 
+        l, r = 0, len(nums)-1
+        res = []
+        flag = 0
+        while l<=r:
+            if flag == 0: 
+                res.append(nums[l])
+                l += 1
+                flag = 1
+            else: 
+                res.append(nums[r])
+                r -= 1
+                flag = 0 
+        cur = dummy = ListNode(0)
+        for e in res:
+            cur.next = ListNode(e)
+            cur = cur.next
         
-        # merge 2 halves 
-        first, second = head, prev
-        while second: 
-            tmp1, tmp2 = first.next, second.next 
-            first.next = second 
-            second.next = tmp1
-            first, second = tmp1, tmp2
-        
+        head.next = dummy.next.next
+        # print(head)
+        # return dummy.next
