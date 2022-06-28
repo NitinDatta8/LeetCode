1435e87f6d179eeac7b39d36443403f51c9f098f
Nitin Datta <movvand@gmail.com>
2022-06-28 00:09:24 -0700

    Time: 3385 ms (23.73%), Space: 106.3 MB (56.20%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,37 +1,20 @@
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    def validPath(self, n: int, edges: List[List[int]], src: int, dst: int) -> bool:
+        graph = defaultdict(list)
+        queue = deque()
+        for node1, node2 in edges: 
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+            
+        queue.append(src)
         visited = set()
-        
-        def build_graph(edges):
-            graph = {}
-            for e in edges: 
-                a, b = e
-                if a not in graph: 
-                    graph[a] = []
-                if b not in graph: 
-                    graph[b] = []
-                
-                graph[a].append(b)
-                graph[b].append(a)
-            
-            return graph 
-        
-        def dfs(graph, src, dst, visited):
-            if src == dst: 
+        while queue: 
+            cur_node = queue.popleft()
+            if cur_node == dst: 
                 return True 
-            if src in visited: 
-                return False 
-            
-            visited.add(src)
-            
-            for neighbor in graph[src]:
-                if dfs(graph, neighbor, dst, visited) == True: 
-                    return True 
-            
-            return False 
+            visited.add(cur_node)
+            for neighbor in graph[cur_node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
         
-        # Build the graph 
-        graph = build_graph(edges)
-        
-        # Run DFS 
-        return dfs(graph, source, destination, visited)
+        return False
