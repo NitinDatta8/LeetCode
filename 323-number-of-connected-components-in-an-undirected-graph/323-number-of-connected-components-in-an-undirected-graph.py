1e169a9f4010cef97e80f5f5d5678c636cf342c0
Nitin Datta <movvand@gmail.com>
2022-06-28 00:45:07 -0700

    Time: 222 ms (12.96%), Space: 15.6 MB (44.43%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,28 @@
+# Create a adjacency list 
+# iterate over each element of adjacency list and do a dfs/bfs and mark visited nodes 
+# once dfs/bfs is completed increment count by 1
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for node1, node2 in edges: 
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+        
+        seen = set()
+        count = 0
+        
+        def bfs(node):
+            queue = [node]
+            for n in queue:
+                for nei in graph[n]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        queue.append(nei)
+        
+        for i in range(n):
+            if i not in seen:
+                bfs(i)
+                count += 1
+        
+        return count
