b3542ec5ca21d21e7c378ac882d9cd72479b5e87
Nitin Datta <movvand@gmail.com>
2022-07-04 10:32:31 -0700

    Time: 183 ms (29.85%), Space: 15.5 MB (67.46%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -9,20 +9,19 @@ class Solution:
             graph[node1].append(node2)
             graph[node2].append(node1)
         
-        seen = set()
+        visited = set()
         count = 0
-        
         def bfs(node):
             queue = [node]
-            for n in queue:
-                for nei in graph[n]:
-                    if nei not in seen:
-                        seen.add(nei)
+            for n in queue: 
+                for nei in graph[n]: 
+                    if nei not in visited: 
+                        visited.add(nei)
                         queue.append(nei)
         
+        
         for i in range(n):
-            if i not in seen:
+            if i not in visited: 
                 bfs(i)
                 count += 1
-        
         return count
