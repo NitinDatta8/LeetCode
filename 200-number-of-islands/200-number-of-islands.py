7885f67a03cbcfe71c41e31c7c68510d85952bd5
Nitin Datta <movvand@gmail.com>
2022-07-05 14:44:15 -0700

    Time: 673 ms (11.94%), Space: 16.6 MB (41.40%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,46 +1,31 @@
 '''
-loop through every element of the grid
-call explore function if grid[r][c] is not equal to "0"
-check if the grid[r][c] is not visited: 
-and set count to 1
-
-return the count
-
-EXPLORE function
-check for edge cases like right left top down  boundaries
-call BFS on the current cell 
-mark it as visited and explore the whole island and mark them as visited
-
-
+run 2 for loops 
+check if grid[r][c] == 1: 
+run dfs(r,c)
+mark the whole island visited by setting grid[r][c] = 0 
+after whole island is done increment count 
+return count 
 '''
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
         count = 0 
-        for r in range(len(grid)): 
-            for c in range(len(grid[0])):
-                cur = grid[r][c]
-                # cur_name = str(r) + ',' + str(c)
-                if self.explore(grid,r,c,visited) == True:
+        ROWS, COLS = len(grid), len(grid[0])
+        def dfs(r,c): 
+            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == '0': 
+                return 
+            
+            grid[r][c] = '0'
+            dfs(r-1, c)
+            dfs(r+1, c)
+            dfs(r, c-1)
+            dfs(r, c+1)
+            return 
+            
+            
+        for r in range(ROWS): 
+            for c in range(COLS): 
+                if grid[r][c] == '1': 
+                    dfs(r, c)
                     count += 1
-        
-        return count 
-    
-    def explore(self, grid, r, c, visited):
-        cur_name = str(r) + ',' + str(c)
-        rowinbounds = 0 <= r and r < len(grid)
-        colinbounds = 0 <= c and c < len(grid[0])
-        
-        if not rowinbounds or not colinbounds: 
-            return False 
-        
-        if grid[r][c] == "0": return False 
-        
-        if cur_name in visited: return False 
-        visited.add(cur_name)
-        
-        self.explore(grid, r-1, c, visited)
-        self.explore(grid, r+1, c, visited)
-        self.explore(grid, r, c-1, visited)
-        self.explore(grid, r, c+1, visited)
-        return True
+        return count        
