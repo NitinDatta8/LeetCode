0498d3f14810b5580377dc2084808ffd478a1b84
Nitin Datta <movvand@gmail.com>
2022-07-09 17:13:31 -0700

    Time: 336 ms (86.04%), Space: 16.5 MB (63.97%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,31 +1,45 @@
 '''
-run 2 for loops 
-check if grid[r][c] == 1: 
-run dfs(r,c)
-mark the whole island visited by setting grid[r][c] = 0 
-after whole island is done increment count 
-return count 
+count = 0 
+2 for loops to check the grid
+if grid[r][c] == '1':
+    dfs(r,c)
+    count += 1
+
+return count
+
+dfs(r,c):
+check for boundary conditions and grid[r][c] !='0' return 
+grid[r][c] = '0'
+
+dfs(r+1, c)
+dfs(r-1, c)
+dfs(r, c+1)
+dfs(r, c-1)
+return
+
+Time - O(m*n)
+Space - O(1)
 '''
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0 
-        ROWS, COLS = len(grid), len(grid[0])
-        def dfs(r,c): 
-            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == '0': 
+        
+        def dfs(r,c):
+            if r < 0 or c < 0 or r == len(grid) or c == len(grid[0]) or grid[r][c] == '0':
                 return 
             
             grid[r][c] = '0'
-            dfs(r-1, c)
             dfs(r+1, c)
-            dfs(r, c-1)
+            dfs(r-1, c)
             dfs(r, c+1)
+            dfs(r, c-1)
             return 
             
-            
-        for r in range(ROWS): 
-            for c in range(COLS): 
-                if grid[r][c] == '1': 
-                    dfs(r, c)
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    dfs(r,c)
                     count += 1
-        return count        
+        return count
