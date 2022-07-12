3ba13abf80a593b7c6aac1547355588ce585f9e6
Nitin Datta <movvand@gmail.com>
2022-07-11 19:43:21 -0700

    Time: 104 ms (18.49%), Space: 13.9 MB (91.33%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,41 +1,38 @@
+
 '''
-use BFS 
+use BFS
 maintain time and fresh oranges
-in first 2 loops count fresh oranges and add rotten oranges to a queue
-run the queue and popleft and use for loop to check adjacent cells 
-change adjacent 1s to 2s and append to queue 
-reduce fresh 
+use 2 for loops to iterate over every cell of grid and keep count of fresh oranges at the same time add rotten oranges to queue
+run over queue and popleft 
+use for loop and check adjacent cells mark 1s to 2s and append to queue and reduce fresh 
 increase time 
-return time if fresh == 0 else -1
+return time if fresh == 0 or -1
 '''
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        time, fresh = 0, 0
+        time = 0 
+        fresh = 0 
         q = deque()
-        
-        for r in range(ROWS): 
+        ROWS, COLS = len(grid), len(grid[0])        
+        for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1: 
                     fresh += 1
-                
-                if grid[r][c] == 2: 
+                elif grid[r][c] == 2: 
                     q.append([r,c])
         
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        while q and fresh > 0:
+        while q and fresh > 0: 
             for i in range(len(q)):
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c]==0 or grid[r][c]==2: 
+                r, c = q.popleft()
+                for dr, dc in directions: 
+                    row, col = r + dr, c + dc
+                    if row < 0 or col < 0 or row == ROWS or col == COLS or grid[row][col] != 1:
                         continue
-
-                    grid[r][c] = 2
-                    q.append([r,c])
+                    
+                    grid[row][col] = 2
+                    q.append([row,col])
                     fresh -= 1
             time += 1
         
         return time if fresh == 0 else -1
-        
- 
