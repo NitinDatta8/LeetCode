40e08ea5b0a4da39a636a7b403d7bfd1ea87f997
Nitin Datta <movvand@gmail.com>
2022-07-19 16:47:35 -0700

    Time: 123 ms (76.76%), Space: 17.6 MB (21.89%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,43 +1,45 @@
 '''
-1. put into adjacency list 
-2. run dfs on every course 
-3. DFS()
-    4. check if crs in cycle: return False 
-    5. cehck if crs in visit: return True 
-    add to cycle 
-    run dfs on prereqs
-    if dfs(pre) == False: return False 
-    
-    remove crs from cycle because there isnt one 
-    add crs to visit 
-    add crs to output
-    return true
+create an adjacency list to store course as key and its prereqs as values (in a list)
+visit, cycle empty sets
+
+DFS(course)
+to check base condition check if course in cycle return false and if it is in visit return true 
+add current course to cycle 
+run dfs on its prereqs and if dfs(prereq) is False return False 
+remove from cycle 
+add in visit
+add to ouput 
+return true 
+
+iterate over all courses and do dfs 
+if anyone return false return []
+return output at end 
 '''
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = {i:[] for i in range(numCourses)}
-        
         for crs, pre in prerequisites: 
             graph[crs].append(pre)
         
         output = []
         cycle, visit = set(), set()
         def dfs(crs): 
-            if crs in visit: 
-                return True
             if crs in cycle: 
                 return False 
+            if crs in visit: 
+                return True 
             
             cycle.add(crs)
-            for pre in graph[crs]:
-                if dfs(pre) == False:
+            for pre in graph[crs]: 
+                if dfs(pre) == False: 
                     return False 
+            
             cycle.remove(crs)
             visit.add(crs)
             output.append(crs)
             return True
-                
-        for crs in range(numCourses):
+        
+        for crs in range(numCourses): 
             if dfs(crs) == False: 
                 return []
         
