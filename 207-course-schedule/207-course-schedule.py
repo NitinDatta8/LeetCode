0abc2600c683d74ed3d4ab544412429a45b05ec4
Nitin Datta <movvand@gmail.com>
2022-07-19 16:25:30 -0700

    Time: 98 ms (97.37%), Space: 17 MB (39.56%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,10 +1,26 @@
+'''
+convert the prerequisites to adjacency list using hashmap 
+use for loop iterating and do dfs for each course 
+create a visited set
+DFS()
+if course in visted return False 
+if course doesnt have any prereq return True 
+add course to visited 
+traverse prereqs for current course and do dfs
+if not dfs(pre) return False 
+
+remove course from visited 
+set prereqs for current course to empty 
+return True 
+
+'''
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = {i:[] for i in range(numCourses)}
         
-        for pre, crs in prerequisites: 
+        for crs, pre in prerequisites: 
             graph[crs].append(pre)
-            
+        
         visit = set()
         def dfs(crs):
             if crs in visit: 
@@ -13,7 +29,7 @@ class Solution:
                 return True 
             
             visit.add(crs)
-            for pre in graph[crs]:
+            for pre in graph[crs]: 
                 if not dfs(pre): 
                     return False 
             
@@ -21,7 +37,7 @@ class Solution:
             graph[crs] = []
             return True 
         
-        for crs in range(numCourses): 
+        for crs in range(numCourses):
             if not dfs(crs): 
                 return False 
         
