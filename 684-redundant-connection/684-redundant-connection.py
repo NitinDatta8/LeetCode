973578b385cd76a6ad6576b883eb539e8b2cc4fb
Nitin Datta <movvand@gmail.com>
2022-07-19 17:10:37 -0700

    Time: 73 ms (72.97%), Space: 14.3 MB (93.31%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -6,14 +6,15 @@ class Solution:
         def find(n): 
             p = par[n]
             
-            while p != par[p]:
-                par[p] = par[par[p]] # path compression (makes it faster)
+            while p != par[p]: 
+                par[p] = par[par[p]]  # path compression (makes the algorithm slightly faster)
                 p = par[p]
-                
+            
             return p 
-        
+    
         def union(n1, n2): 
             p1, p2 = find(n1), find(n2)
+            
             if p1 == p2: 
                 return False 
             
@@ -23,8 +24,9 @@ class Solution:
             else: 
                 par[p1] = p2 
                 rank[p2] += rank[p1]
-            return True 
+            
+            return True
         
         for n1, n2 in edges: 
             if not union(n1, n2): 
-                return [n1,n2]
+                return [n1, n2]
