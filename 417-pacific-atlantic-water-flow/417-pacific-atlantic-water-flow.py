3a86bebfc773e51ae39cdc08853c240d5e66a157
Nitin Datta <movvand@gmail.com>
2022-07-21 16:50:54 -0700

    Time: 295 ms (91.22%), Space: 15.9 MB (21.65%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,8 +1,12 @@
+'''
+
+'''
+
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
         
         pac, atl = set(), set()
+        ROWS, COLS = len(heights), len(heights[0])
         
         def dfs(r, c, visited, prev_height):
             if r < 0 or c < 0 or r == ROWS or c == COLS or heights[r][c] < prev_height or (r,c) in visited: 
@@ -20,11 +24,14 @@ class Solution:
         
         for c in range(COLS): 
             dfs(0, c, pac, heights[0][c])
-            dfs(ROWS-1, c,  atl, heights[ROWS-1][c])
-        
+            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
+            
         res = []
         for r in range(ROWS): 
             for c in range(COLS): 
-                if (r,c) in atl and (r,c) in pac: 
+                if (r,c) in pac and (r,c) in atl: 
                     res.append([r,c])
+        
         return res
+            
+            
