53354fd75f6cdefb0c1b6c87c2580c6445dd1f92
Nitin Datta <movvand@gmail.com>
2022-07-23 16:48:58 -0700

    Time: 103 ms (93.11%), Space: 17 MB (39.56%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,44 +1,34 @@
 '''
-convert the prerequisites to adjacency list using hashmap 
-use for loop iterating and do dfs for each course 
-create a visited set
-DFS()
-if course in visted return False 
-if course doesnt have any prereq return True 
-add course to visited 
-traverse prereqs for current course and do dfs
-if not dfs(pre) return False 
-
-remove course from visited 
-set prereqs for current course to empty 
-return True 
-
+1. create adjacency list 
+2. iterate over every course and maintain visit set 
+3. dfs() - recursively go through prereqs and check if they satisfy prereq condition
+    visite
+   {1:[], 2:[], 3:[]} 
 '''
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {i:[] for i in range(numCourses)}
-        
+        adj = {i:[] for i in range(numCourses)}
         for crs, pre in prerequisites: 
-            graph[crs].append(pre)
+            adj[crs].append(pre)
         
         visit = set()
         def dfs(crs):
             if crs in visit: 
                 return False 
-            if graph[crs] == []: 
+            if adj[crs] == []: 
                 return True 
             
             visit.add(crs)
-            for pre in graph[crs]: 
+            for pre in adj[crs]: 
                 if not dfs(pre): 
                     return False 
             
             visit.remove(crs)
-            graph[crs] = []
-            return True 
+            adj[crs] = []
+            return True
+            
         
-        for crs in range(numCourses):
+        for crs in range(numCourses): 
             if not dfs(crs): 
                 return False 
-        
         return True
