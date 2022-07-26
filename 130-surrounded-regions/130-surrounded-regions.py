88a0b943a453cc23f22e2798f6efb52d06e09ee8
Nitin Datta <movvand@gmail.com>
2022-07-26 15:35:32 -0700

    Time: 280 ms (25.11%), Space: 15.8 MB (71.93%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -1,14 +1,8 @@
 '''
-run 2 for loops and iterate over every cell 
-if cell is 'O' and is on the border run a DFS
-DFS is used to mark the cells connected to border 'O' cell 
+check boundaries and run dfs if value is O 
+to mark them set them as V 
 
-in DFS()
-    check for border conditions and check if cell is not marked 'X' and return for these cases 
-    mark current cell as 'V' 
-    run dfs on all 4 directions
-    
-run 2 for loops again and convert all 'O's to X and all 'V's to 'O'
+at end go through every cell and check if its V mark them as O other all mark as X
 '''
         
 class Solution:
@@ -18,26 +12,24 @@ class Solution:
         """
         ROWS, COLS = len(grid), len(grid[0])
         
-        def dfs(r,c): 
-            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] != 'O':
+        def dfs(r, c): 
+            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] != 'O': 
                 return 
-            grid[r][c] = 'V'
-            dfs(r + 1,c)
-            dfs(r - 1,c)
-            dfs(r,c + 1)
-            dfs(r,c - 1)
-            
-            
-            
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 'O' and (r in [0, ROWS-1] or c in [0,COLS-1]): 
-                    dfs(r,c)
-                    
-        for r in range(ROWS):
-            for c in range(COLS): 
-                if grid[r][c] == 'O':
-                    grid[r][c] = 'X'
-                elif grid[r][c] == 'V':
-                    grid[r][c] = 'O'
+            grid[r][c] = 'V' 
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
         
+        for r in range(ROWS): 
+            for c in range(COLS): 
+                if r in [0, ROWS-1] or c in [0, COLS-1]: 
+                    if grid[r][c] == 'O': 
+                        dfs(r,c)
+                    
+        for r in range(ROWS): 
+            for c in range(COLS): 
+                if grid[r][c] == 'V':
+                    grid[r][c] = 'O'
+                else: 
+                    grid[r][c] = 'X'
