47b1eee6709621348ae648daf513faaf4dabd50b
Nitin Datta <movvand@gmail.com>
2022-08-03 00:02:32 -0700

    Time: 152 ms (81.27%), Space: 16.1 MB (17.75%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,34 +1,25 @@
-# 4 2 0 3 2 5 
-# 0 4 4 4 4 4 max_left 
-# 5 5 5 5 5 0 max_right
-# 0 4 4 4 4 0 x = min(max_left, max_right)
-# 0 2 4 1 2 0 x - height[i]
+'''
+start from left and maintain max_left till that point 
+start from right and maintain max_right till that point
+take min of these 2 at certain index and remove the height from it 
+'''
 class Solution:
     def trap(self, nums: List[int]) -> int:
-        # step 1 
-        max_left = [0] * len(nums)
-        maxl = 0 
-        for i in range(1,len(nums)):
-            maxl = max(maxl,nums[i-1])
-            max_left[i] = maxl
+        max_left = [1] * len(nums)
+        max_right = [1] * len(nums)
+        max_l, max_r = 0, 0 
+        for i in range(len(nums)): 
+            max_l = max(max_l, nums[i])
+            max_left[i] = max_l 
         
-        # step 2
-        max_right = [0] * len(nums)
-        maxr = 0 
-        for i in range(len(nums)-2, -1, -1 ):
-            maxr = max(maxr,nums[i+1] )
-            max_right[i] = maxr
+        for i in range(len(nums)-1, -1, -1): 
+            max_r = max(max_r, nums[i])
+            max_right[i] = max_r
         
-        # step 3
-        min_both = []
-        for i,j in zip(max_left, max_right):
-            min_both.append(min(i,j))
-        
-        res = []
-        for i,j in zip(min_both,nums):
-            val = i - j 
-            if val < 0: 
-                res.append(0)
-            else: 
-                res.append(val)
-        return sum(res)
+        ans = 0
+        for i in range(len(nums)): 
+            min_val = min(max_left[i], max_right[i])
+            ans += min_val - nums[i]
+            
+        return ans
+            
