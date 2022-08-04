c75284c320b4da96e5a6e96904be4741c0a8f088
Nitin Datta <movvand@gmail.com>
2022-08-04 15:36:38 -0700

    Time: 80 ms (79.96%), Space: 14.1 MB (22.67%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -17,18 +17,19 @@ class Solution:
         
         def backtrack(pos, cur, target): 
             if target == 0: 
-                res.append(cur.copy())
+                res.append(cur[:])
             if target <= 0: 
                 return 
             
             prev = -1
             for i in range(pos, len(candidates)): 
-                if candidates[i] == prev:
+                if candidates[i] == prev: 
                     continue
+                
                 cur.append(candidates[i])
                 backtrack(i + 1, cur, target - candidates[i])
                 cur.pop()
                 prev = candidates[i]
-            
+        
         backtrack(0, [], target)
         return res
