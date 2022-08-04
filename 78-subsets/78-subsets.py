2db02380cab16d58c1942dc9b5577b7cef109323
Nitin Datta <movvand@gmail.com>
2022-08-04 13:46:41 -0700

    Time: 42 ms (79.05%), Space: 14.1 MB (35.35%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -9,21 +9,16 @@ class Solution:
         res = []
         subset = []
         
-        def dfs(i): 
-            # base case
-            if i >= len(nums): 
-                res.append(subset.copy())
+        def backtrack(i): 
+            if i >= len(nums):
+                res.append(subset[:])
                 return 
             
-            # decision to include nums[i]
             subset.append(nums[i])
-            dfs(i + 1)
+            backtrack(i + 1)
             
-            # decision to not include nums[i]
             subset.pop()
-            dfs(i + 1)
+            backtrack(i + 1)
         
-        dfs(0)
+        backtrack(0)
         return res
-        
-    
