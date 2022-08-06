f6a892813541544ea02799517350d2c91f375632
Nitin Datta <movvand@gmail.com>
2022-08-06 11:46:34 -0700

    Time: 188 ms (19.03%), Space: 14.1 MB (72.82%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -5,22 +5,19 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def dfs(i, cur, total): 
+        def backtrack(i, cur, total):
             if total == target: 
-                res.append(cur.copy())
+                res.append(cur[:])
                 return 
             
-            if i >= len(candidates) or total > target: 
+            if total > target or i >= len(candidates): 
                 return 
             
-            # Case 1 where we add the element
             cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
+            backtrack(i, cur, total + candidates[i])
             
-            # Case 2 go down without adding the current element 
             cur.pop()
-            dfs(i + 1, cur, total)
-        
-        
-        dfs(0, [], 0)
+            backtrack(i + 1, cur, total)
+            
+        backtrack(0, [], 0)
         return res
