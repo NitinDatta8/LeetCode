f6ea87682957d8663c645e0ea39bcfe019b24779
Nitin Datta <movvand@gmail.com>
2022-08-06 12:06:38 -0700

    Time: 127 ms (45.40%), Space: 14.1 MB (22.67%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -23,13 +23,13 @@ class Solution:
             
             prev = -1
             for i in range(pos, len(candidates)): 
-                if candidates[i] == prev: 
+                if prev == candidates[i]:
                     continue
                 
                 cur.append(candidates[i])
                 backtrack(i + 1, cur, target - candidates[i])
                 cur.pop()
                 prev = candidates[i]
-        
+            
         backtrack(0, [], target)
         return res
