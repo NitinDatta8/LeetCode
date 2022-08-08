3346ae31e56250328a963c3f11a3c349057d956b
Nitin Datta <movvand@gmail.com>
2022-08-08 15:52:20 -0700

    Time: 66 ms (96.54%), Space: 14 MB (43.89%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,13 +10,14 @@ class Solution:
         
         while l1 or l2 or carry: 
             val1 = l1.val if l1 else 0 
-            val2 = l2.val if l2 else 0 
+            val2 = l2.val if l2 else 0
             
             carry, val_sum = divmod(val1 + val2 + carry, 10)
             
             dummy.next = ListNode(val_sum)
-            dummy = dummy.next 
+            dummy = dummy.next
             
             l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            l2 = l2.next if l2 else None 
+        
         return sumlist.next
