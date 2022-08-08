7668d1f0a40432c4c2f21dc28c174eea04090f0e
Nitin Datta <movvand@gmail.com>
2022-08-08 15:35:19 -0700

    Time: 114 ms (73.95%), Space: 23.9 MB (64.25%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -3,50 +3,38 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
+'''
+1. find the middle of list using fast and slow pointer 
+2. reverse the second half of list 
+3. combine first half and second half by modifying the pointers
+'''
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        '''
-        O(n)time  o(1) space
-        slow, fast
-        while fast and fast.next: 
-        
-        1 2 3->N<-4<-5
-        1->5->2
-            s         
-        head1 = 1
-        head2 = 5
-        tmp1 = 2
-        tmp2 = 4
-        
-        '''  
-        # step1: middle of linked list 
-        slow = fast = head 
-        while fast and fast.next: 
-            slow = slow.next
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next 
             fast = fast.next.next
         
-        # step2: reverse second half of the linked list 
-        second = slow.next
-        slow.next = None 
-        prev = None 
+        prev = None
+        second = slow.next 
+        slow.next = None
+        
         while second: 
-            nxt = second.next
-            second.next = prev
+            nxt = second.next 
+            second.next = prev 
             prev = second 
-            second = nxt
+            second = nxt 
         
         head1 = head 
         head2 = prev
         while head2: 
-            tmp1, tmp2 = head1.next, head2.next
-            head1.next = head2
-            head2.next = tmp1
+            tmp1, tmp2 = head1.next, head2.next 
+            head1.next = head2 
+            head2.next = tmp1 
             head1 = tmp1
-            head2 = tmp2 
-        
+            head2 = tmp2
         return head
             
