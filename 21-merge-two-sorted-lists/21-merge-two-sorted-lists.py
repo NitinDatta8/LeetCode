3d391cdb9453e8df8e94f4b6b518af69827ebe8c
Nitin Datta <movvand@gmail.com>
2022-08-08 15:28:07 -0700

    Time: 57 ms (54.91%), Space: 13.8 MB (79.56%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -3,6 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+'''
+l1 1 2 4 
+l2 2 3 4 
+Node(0) -> 1 -> 2  -> 2 -> 3 -> 4 -> 4
+'''
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = cur = ListNode(0)
@@ -12,9 +18,8 @@ class Solution:
                 list1 = list1.next
             else: 
                 cur.next = list2
-                list2 = list2.next
-            cur = cur.next
+                list2 = list2.next 
+            cur = cur.next 
+        
         cur.next = list1 or list2
-            
-        return dummy.next 
-            
+        return dummy.next
