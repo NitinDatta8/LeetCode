b899fe9a8af7e7f670bf7f09262114071a4b330d
Nitin Datta <movvand@gmail.com>
2022-08-08 23:39:18 -0700

    Time: 140 ms (46.87%), Space: 14.2 MB (36.16%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,23 +1,30 @@
 '''
 Time: O(2^target) - the reason is because when we create the tree we can go maximum till the target value where our base case stops if it goes more
+res = []
+backtrack(i, cur, total): 
+    if total == target: 
+        res.append(cur[:])
+        return 
+    
+    cur.append(candidates[i])
+    backtrack(i, cur, total + candidates[i])
+    cur.pop()
+    backtrack(i + 1, cur, total)
+    
 '''
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        
-        def backtrack(i, cur, total):
+        def backtrack(i, cur, total): 
             if total == target: 
                 res.append(cur[:])
                 return 
-            
             if total > target or i >= len(candidates): 
                 return 
-            
             cur.append(candidates[i])
             backtrack(i, cur, total + candidates[i])
-            
             cur.pop()
             backtrack(i + 1, cur, total)
-            
+        
         backtrack(0, [], 0)
         return res
