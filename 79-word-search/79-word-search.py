b3fe74b518cefb4a284a815da614594669c19f92
Nitin Datta <movvand@gmail.com>
2022-08-08 23:17:30 -0700

    Time: 8033 ms (42.35%), Space: 14.1 MB (12.49%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -8,9 +8,10 @@ class Solution:
         
         def dfs(r, c, i): 
             if i == len(word): 
-                return True 
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or word[i] != board[r][c] or (r,c) in path: 
-                return False 
+                return True
+            
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or word[i] != board[r][c] or (r, c) in path: 
+                return False
             
             path.add((r,c))
             res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
@@ -18,7 +19,8 @@ class Solution:
             return res
         
         for r in range(ROWS):
-            for c in range(COLS): 
-                if dfs(r, c, 0): return True 
-        
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+            
         return False
