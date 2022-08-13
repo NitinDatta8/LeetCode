68ec49926d25bb9a68a79f3ab1b70aac8e249ba1
Nitin Datta <movvand@gmail.com>
2022-08-12 17:32:16 -0700

    Time: 113 ms (56.56%), Space: 17.2 MB (78.89%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -1,8 +1,16 @@
+'''
+sort based on start value
+iterate over each interval 
+check if end of current >= start of next: 
+    return False 
+return true at end
+'''
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
-        for i in range(len(intervals)-1):
-            if intervals[i][1] >  intervals[i+1][0]:
+        for i in range(len(intervals) - 1): 
+            if intervals[i][1] > intervals[i + 1][0]: 
                 return False
         return True
-            
+        
+        
