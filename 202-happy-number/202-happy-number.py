9d8f75507fe79e77c225202056729973a69af324
Nitin Datta <movvand@gmail.com>
2022-08-13 12:57:36 -0700

    Time: 70 ms (16.71%), Space: 13.8 MB (97.14%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+        
+        while n not in visit: 
+            visit.add(n)
+            n = self.sum_squares(n)
+            if n == 1: 
+                return True
+        
+        return False 
+        
+    
+    def sum_squares(self, n): 
+        output = 0 
+        while n: 
+            digit = n % 10 
+            digit = digit ** 2
+            output += digit 
+            n = n // 10 
+        return output
