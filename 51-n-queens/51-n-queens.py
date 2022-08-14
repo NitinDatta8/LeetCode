e61b0e400e0adde5850e90652e5f220b53a546d7
Nitin Datta <movvand@gmail.com>
2022-08-14 09:46:19 -0700

    Time: 77 ms (72.56%), Space: 14.5 MB (43.94%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,9 +1,19 @@
+'''
+queen moves in col, row, pos_diag and neg_diag
+use backtracking - 
+    iterate over row and check if (r,c) present in col, pos_diag or neg_diag
+    if present continue
+    add (r,c) to all 3 sets 
+    set (r,c) to Q 
+    backtrack()
+    remove from sets 
+    set (r,c) back to .
+'''
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         cols = set()
         pos_diag = set()
-        neg_diag = set()
-        
+        neg_diag =set()
         res = []
         board = [['.'] * n for i in range(n)]
         def backtrack(r): 
@@ -14,7 +24,7 @@ class Solution:
             
             for c in range(n): 
                 if c in cols or (r + c) in pos_diag or (r - c) in neg_diag: 
-                    continue 
+                    continue
                 
                 cols.add(c)
                 pos_diag.add(r + c)
@@ -22,12 +32,10 @@ class Solution:
                 board[r][c] = 'Q'
                 
                 backtrack(r + 1)
-                
                 cols.remove(c)
                 pos_diag.remove(r + c)
                 neg_diag.remove(r - c)
                 board[r][c] = '.'
-            
+        
         backtrack(0)
         return res
-                
