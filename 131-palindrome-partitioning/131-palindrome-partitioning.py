319fa9f28174578d3400932d0715e947a7d7e851
Nitin Datta <movvand@gmail.com>
2022-08-14 10:13:22 -0700

    Time: 880 ms (66.72%), Space: 30.4 MB (43.41%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,9 +1,29 @@
+'''
+res = []
+part = []
+backtrack(i): 
+    base case - i >= len(s): 
+    res.append(part[:])
+    return 
+    
+    for j in range(i+1, len(s)):
+        if self.ispali(s,i, j):
+            part.append(s[i:j+1])
+            backtrack(j + 1)
+            part.pop()
+    
+    backtrack(0)
+    return res
+    
+pali(s, l, r): 
+'''
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        part = []
         res = []
+        part = []
         
         def backtrack(i): 
+            # Base case 
             if i >= len(s): 
                 res.append(part[:])
                 return 
@@ -11,7 +31,7 @@ class Solution:
             for j in range(i, len(s)): 
                 if self.ispali(s, i, j): 
                     part.append(s[i:j + 1])
-                    backtrack(j + 1)   
+                    backtrack(j + 1)
                     part.pop()
         
         backtrack(0)
