da3844022b137a10b98c225b1c18687e0d42ac27
Nitin Datta <movvand@gmail.com>
2022-08-14 10:25:09 -0700

    Time: 9337 ms (13.52%), Space: 13.9 MB (51.13%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,5 +1,12 @@
 '''
 Time O(n m 4^len(word))
+iterate over every cell and do dfs 
+dfs()
+    base case - if i == len(word): return True 
+    check for boundary conditions and if (r,c) already in set and if word[i] is not the same char at board: continue 
+    res = dfs in 4 directions 
+
+return False
 '''
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
@@ -8,19 +15,20 @@ class Solution:
         
         def dfs(r, c, i): 
             if i == len(word): 
-                return True
+                return True 
             
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or word[i] != board[r][c] or (r, c) in path: 
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] != word[i] or (r,c) in path: 
                 return False
             
             path.add((r,c))
             res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
-            path.remove((r,c))
+            path.remove((r,c))            
             return res
         
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(ROWS): 
+            for c in range(COLS): 
                 if dfs(r, c, 0):
                     return True
+        
+        return False 
             
-        return False
