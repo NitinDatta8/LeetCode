4ea2f821137d4e091b5b521d7ef85557bd48c0a8
Nitin Datta <movvand@gmail.com>
2022-08-16 10:55:32 -0700

    Time: 8759 ms (26.12%), Space: 13.9 MB (92.88%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,34 +1,35 @@
 '''
-Time O(n m 4^len(word))
-iterate over every cell and do dfs 
-dfs()
-    base case - if i == len(word): return True 
-    check for boundary conditions and if (r,c) already in set and if word[i] is not the same char at board: continue 
-    res = dfs in 4 directions 
-
-return False
+time O(nm 4^len(word))
+iterate over every cell of board and do dfs
+if dfs(): return True 
+dfs(r, c, i): 
+    check base case - if i == len(word): return True
+    check boundary conditions and word[i]!= board[r][c] and (r,c) should not be visited (to avoid infinite loops) - return False 
+    add current coordinates to visited set
+    do dfs in all 4 directions 
+    return res
+return False 
 '''
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        path = set()
+        visited = set()
         
         def dfs(r, c, i): 
             if i == len(word): 
                 return True 
             
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] != word[i] or (r,c) in path: 
-                return False
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c]!= word[i] or (r,c) in visited: 
+                return False 
             
-            path.add((r,c))
+            visited.add((r,c))
             res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
-            path.remove((r,c))            
+            visited.remove((r,c))
             return res
         
-        for r in range(ROWS): 
+        for r in range(ROWS):
             for c in range(COLS): 
                 if dfs(r, c, 0):
-                    return True
-        
-        return False 
-            
+                    return True 
+        return False
+                    
