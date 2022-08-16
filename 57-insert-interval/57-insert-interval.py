22566297c1f4efccef88f234879b2bc4989ce467
Nitin Datta <movvand@gmail.com>
2022-08-16 10:29:36 -0700

    Time: 85 ms (92.72%), Space: 17.4 MB (52.80%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,19 +1,26 @@
 '''
-   2----------5
+need to check 3 main condition 
+no overlap and smaller than current interval 
+    put newinterval into result and put rest all intervals after it 
+no overlap and bigger than current interval 
+    put current interval into result
+overlap condition
+    modify newinterval = min(newinterval[0], interval[0]), max(newinterval[1], interval[1])
 
-1------3          6------9
+at end add newinterval and return
+we do not add newinterval as soon as its modified because it is possible that they might be merged again incase of overlap
 '''
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
         for i in range(len(intervals)): 
-            if newInterval[1] < intervals[i][0]:  # no overlap
+            if newInterval[1] < intervals[i][0]: 
                 res.append(newInterval)
-                return res +  intervals[i:]
-            elif newInterval[0] > intervals[i][1]: # no overlap
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]: 
                 res.append(intervals[i])
             else: 
-                newInterval = [min(newInterval[0], intervals[i][0]),  max(newInterval[1], intervals[i][1])]
+                newInterval = [min(newInterval[0],intervals[i][0]), max(newInterval[1], intervals[i][1])]
             
         res.append(newInterval)
         return res
