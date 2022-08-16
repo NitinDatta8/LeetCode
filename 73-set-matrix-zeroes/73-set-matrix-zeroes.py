0389c490c4ccbbb3e5f4d985a883d662eda05851
Nitin Datta <movvand@gmail.com>
2022-08-16 10:03:27 -0700

    Time: 221 ms (39.63%), Space: 14.9 MB (16.28%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,31 +3,32 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # O(1)
         ROWS, COLS = len(matrix), len(matrix[0])
-        row_zero = False 
+        row_zero = False
         
-        # determine which rows/cols need to be set to 0
         for r in range(ROWS):
             for c in range(COLS): 
                 if matrix[r][c] == 0: 
-                    matrix[0][c] = 0
+                    # set top row to 0
+                    matrix[0][c] = 0 
+                    
                     if r > 0: 
+                        # set left column to 0 
                         matrix[r][0] = 0 
                     else: 
-                        row_zero = True 
+                        row_zero = True
         
-        for r in range(1, ROWS): 
-            for c in range(1, COLS): 
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
                 if matrix[0][c] == 0 or matrix[r][0] == 0: 
                     matrix[r][c] = 0 
         
+        # set left column to 0
         if matrix[0][0] == 0: 
-            for r in range(ROWS): 
-                matrix[r][0] = 0
+            for r in range(ROWS):
+                matrix[r][0] = 0 
         
-        if row_zero:
+        # set top row to 0
+        if row_zero: 
             for c in range(COLS): 
                 matrix[0][c] = 0 
-                
-                
