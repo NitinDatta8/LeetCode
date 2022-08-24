17ba80626ff526b605e72bf0b1391ad3dd49d56e
Nitin Datta <movvand@gmail.com>
2022-08-23 18:39:16 -0700

    Time: 508 ms (31.38%), Space: 20.8 MB (25.40%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -1,3 +1,11 @@
+'''
+1. iterate over every cell and store where are the gates  and store them in a queue - (r,c)
+2. BFS(): 
+    check for boundary conditions and check for walls and check for visited condition
+    pop left element from queue and traverse in 4 directions and maintain distance and replace inf with the dist
+
+q = [(0,2) (3,0)]
+'''
 
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
@@ -7,28 +15,28 @@ class Solution:
         q = deque()
         visited = set()
         ROWS, COLS = len(rooms), len(rooms[0])
-        
-        def addrooms(r,c):
-            if r < 0 or c < 0 or r == ROWS or c == COLS or rooms[r][c] == -1 or (r,c) in visited: 
-                return 
-            visited.add((r,c))
-            q.append([r,c])
-                
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if rooms[r][c] == 0:
+        for r in range(ROWS): 
+            for c in range(COLS): 
+                if rooms[r][c] == 0: 
                     q.append([r,c])
                     visited.add((r,c))
         
-        
-        dist = 0
-        while q: 
-            for _ in range(len(q)):
+        dist = 1
+        while len(q) > 0: 
+            length = len(q)
+            for i in range(length):
                 r, c = q.popleft()
-                rooms[r][c] = dist 
-                addrooms(r+1, c)
-                addrooms(r-1, c)
-                addrooms(r, c+1)
-                addrooms(r, c-1)
+                
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions: 
+                    row, col = r + dr, c + dc
+        
+                    if row < 0 or col < 0 or row >= ROWS or col >= COLS or rooms[row][col] == -1 or (row,col) in visited: 
+                        continue
+                    
+                    rooms[row][col] = dist 
+                    visited.add((row, col))
+                    q.append([row, col])
             dist += 1
+
+                
