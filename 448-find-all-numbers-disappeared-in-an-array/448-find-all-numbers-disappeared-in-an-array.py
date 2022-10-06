27cd59200005d20f232d01126fa0a1502e62ab2b
Nitin Datta <movvand@gmail.com>
2022-10-06 00:07:41 -0700

    Time: 409 ms (84.22%), Space: 21.9 MB (76.67%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,8 +1,16 @@
+'''
+enumerate over the list 
+find element in current index and go to that index-1 and set it as negative
+'''
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        n = set(nums)
         res = []
-        for i in range(1,len(nums)+1):
-            if i not in n: 
-                res.append(i)
+        for i,v in enumerate(nums):
+            nums[abs(v)-1] = abs(nums[abs(v)- 1]) *(-1)
+        
+        for i in range(len(nums)): 
+            if nums[i] > 0: 
+                res.append(i+1)
+        
         return res
