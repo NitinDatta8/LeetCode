a29d86c30ea247d4ed031763808cb511b5bcc530
Nitin Datta <movvand@gmail.com>
2022-10-06 18:20:53 -0700

    Time: 86 ms (99.77%), Space: 13.8 MB (82.34%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,19 +1,28 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
+        '''
+        create 3 sets 
+        iterate over every cell using 2 for loops 
+        if . then continue 
+        check if already in any one of the three set return False 
+        add to all the sets 
+        end return true
+        '''
         rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set) # key = (r/3, c/3)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
         
-        for r in range(9): 
-            for c in range(9): 
-                if board[r][c] =='.':
-                    continue
+        for r in range(len(board)): 
+            for c in range(len(board[0])): 
+                if board[r][c] == '.':
+                    continue 
                 
-                if (board[r][c] in rows[r] or 
-                   board[r][c] in cols[c] or 
-                   board[r][c] in squares[(r // 3, c // 3)]): 
+                if (board[r][c] in rows[r]
+                   or board[r][c] in cols[c] or 
+                   board[r][c] in squares[r//3, c//3]): 
                     return False 
-                cols[c].add(board[r][c])
+                
                 rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[r//3,c//3].add(board[r][c])
         return True
