7d89b0eadc41f700f45c5ea75637edb838c83dc2
Nitin Datta <movvand@gmail.com>
2023-10-18 07:09:11 -0700

    Time: 209 ms (66.09%), Space: 57.2 MB (22.41%) - LeetHub

--- 1672-find-the-index-of-the-large-integer/1672-find-the-index-of-the-large-integer.py
+++ 1672-find-the-index-of-the-large-integer/1672-find-the-index-of-the-large-integer.py
@@ -16,48 +16,54 @@
 
 class Solution:
     def getIndex(self, reader: 'ArrayReader') -> int:
-        # s = 0 
-        # e = reader.length() - 1
-        # while s <= e: 
-        #     m = (s + e) // 2
-        #     length = e - s
-        #     print(s, e, m, length)
-        #     if length == 0: 
-        #         return s
-        #     if length % 2 == 0: 
-        #         val = reader.compareSub(s, m - 1, m + 1, e)
-        #         if val == 0: 
-        #             return m 
-        #     else: 
-        #         val = reader.compareSub(s, m, m + 1, e)
-        #     if val == -1: 
-        #         s = m + 1
-        #     elif val == 1: 
-        #         e = m - 1
-        n = reader.length()
-        s = 0
-        e = n-1
-        while s<=e:
-            m=(s+e)//2
-            length = e - s + 1
-            if length % 2 == 0:
-                x = m+1 if m+1 <= e else e
-                print("e",s,m,x,e)
-                res = reader.compareSub(s,m,x,e)
-                if res == 0:
-                    return m
-                elif res == 1:
-                    e = m
-                else:
+        s = 0 
+        e = reader.length() - 1
+        while s <= e: 
+            m = (s + e) // 2
+            length = e - s 
+            print(s, e, m, length)
+            if length == 0: 
+                return s
+            if length % 2 == 0: 
+                val = reader.compareSub(s, m - 1, m + 1, e)
+                if val == 0: 
+                    return m 
+                elif val == -1: 
                     s = m + 1
-            else:
-                r = m-1 if m-1 >= s else s
-                x = m+1 if m+1 <= e else e
-                print("o", s,r,x,e)
-                res = reader.compareSub(s,r,x,e)
-                if res == 0:
-                    return m
-                elif res == 1:
+                elif val == 1: 
                     e = m - 1
-                else:
+            else: 
+                val = reader.compareSub(s, m, m + 1, e)
+                if val == 0:
+                    return m
+                elif val == -1: 
                     s = m + 1
+                elif val == 1: 
+                    e = m
+        # n = reader.length()
+        # s = 0
+        # e = n-1
+        # while s<=e:
+        #     m=(s+e)//2
+        #     length = e - s + 1
+        #     if length % 2 == 0:
+        #         x = m+1 if m+1 <= e else e
+        #         print("e",s,m,x,e)
+        #         res = reader.compareSub(s,m,x,e)
+        #         if res == 0:
+        #             return m
+        #         elif res == 1:
+        #             e = m
+        #         else:
+        #             s = m + 1
+        #     else:
+        #         r = m-1 if m-1 >= s else s
+        #         x = m+1 if m+1 <= e else e
+        #         print("o", s,r,x,e)
+        #         res = reader.compareSub(s,r,x,e)
+        #         if res == 0:
+        #             return m
+        #         elif res == 1:
+        #             e = m - 1
+        #         else:
+        #             s = m + 1
